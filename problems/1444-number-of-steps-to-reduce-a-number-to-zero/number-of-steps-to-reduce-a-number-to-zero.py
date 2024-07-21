535624f605534eb9bdc3453d3600db1d9754343b
Jinsu Lim <dla0830ld@gmail.com>
2024-07-22 02:24:02 +0900

    Time: 34 ms (66.08%) | Memory: 16.3 MB (96.26%) - LeetSync

--- problems/1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
+++ problems/1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,12 +1,11 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        n = num
         i = 0
-        while n > 0:
-            if n % 2 == 0:
-                n = n / 2
+        while num > 0:
+            if num % 2:
+                num -= 1
             else:
-                n = n - 1
+                num /= 2
             i += 1
 
         return i
